6c021c5bd3fba0f558fe027c44eeafc6870cf1a4
Benny <bennychou7573@hotmail.com>
2013-08-29 11:23:20 +0800

    Added localization

--- js/lang/greeting.js
+++ js/lang/greeting.js
@@ -1,4 +1,105 @@
 lang_option_1 = "English";
-lang_option_2 = "Japenese";
-lang_option_3 = "Traditional Chinese";
-lang_option_4 = "Simplified Chinese";
+lang_option_2 = "日文";
+lang_option_3 = "繁體中文";
+lang_option_4 = "簡體中文";
+
+// admin modal
+lang_admin_modal_h3 = "Login";
+lang_admin_modal_label = "Admin's Password";
+lang_admin_modal_button = "Confirm";
+
+// confirm reset or reboot modal
+lang_confirm_modal_cancel_button = "Cancel";
+lang_confirm_modal_confirm_button = "Confirm";
+
+// WDS connect modal
+lang_wds_modal_signal_label = "Signal";
+lang_wds_modal_security_label = "Security";
+lang_wds_modal_mac_label = "Mac Address";
+lang_wds_modal_channel_label = "Channel";
+lang_wds_modal_password_label = "Password";
+lang_wds_modal_wep_label = "Wep Key";
+wds_model_cancel_button = "Cancel";
+wds_model_connect_button = "Connect";
+
+// progerss modal
+lang_count_down_modal_h3 = "Progress...";
+count_down_button = "Refresh";
+
+// status
+lang_status_mode_strong = "Mode:<span id=\"status_mode_span\" class=\"label\"></span>";
+lang_status_mac_strong = "Mac Address:<span id=\"status_mac_span\" class=\"label\"></span>";
+
+// reboot and reset button
+lang_reboot_button = "Reboot";
+lang_reset_button = "Reset";
+
+// nav menu
+lang_nav_ap_bridge_a = "AP Bridge<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+lang_nav_ap_router_a = "AP Router<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+lang_nav_wds_repeater_a = "WDS Repeater<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+lang_nav_wds_bridge_a = "WDS Bridge<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+lang_nav_admin_a = "Admin<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+lang_nav_help_a = "Help<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+
+// ap bridge mode page
+lang_page_ap_bridge_h4 = "AP Bridge Mode";
+ap_bridge_radio_non_security = "Non-Security";
+ap_bridge_radio_security = "Security";
+lang_ap_bridge_ssid_label = "SSID";
+lang_ap_bridge_password_label = "Password";
+lang_ap_bridge_channel_label = "Channel";
+lang_ap_bridge_auto_channel_option = "Auto";
+ap_bridge_radio_ht20 = "HT20";
+ap_bridge_radio_ht40 = "HT40";
+lang_ap_bridge_max_label = "Maximum";
+ap_bridge_static_input = "Static";
+lang_ap_bridge_static_ip = "IP";
+lang_ap_bridge_static_subnet = "Subnet";
+lang_ap_bridge_static_gw = "Gateway";
+lang_ap_bridge_save_button = "Save";
+
+// ap router mode page
+lang_page_ap_router_h4 = "AP Bridge Mode";
+ap_router_radio_non_security = "Non-Security";
+ap_router_radio_security = "Security";
+lang_ap_router_ssid_label = "SSID";
+lang_ap_router_password_label = "Password";
+lang_ap_router_channel_label = "Channel";
+lang_ap_router_auto_channel_option = "Auto";
+ap_router_radio_ht20 = "HT20";
+ap_router_radio_ht40 = "HT40";
+lang_ap_router_max_label = "Maximum";
+ap_router_static_input = "Static";
+lang_ap_router_static_ip = "IP";
+lang_ap_router_static_subnet = "Subnet";
+lang_ap_router_static_gw = "Gateway";
+lang_ap_router_save_button = "Save";
+
+// wds repeater mode
+lang_page_wds_repeater_h4 = "WDS Repeater";
+scan_list_wds_repeater_ssid_th = "SSID";
+scan_list_wds_repeater_sec_th = "Security";
+scan_list_wds_repeater_mac_th = "Mac Address";
+scan_list_wds_repeater_channel_th = "Channel";
+scan_list_wds_repeater_signal_th = "Signal";
+
+// wds bridge mode
+lang_page_wds_bridge_h4 = "WDS Repeater";
+scan_list_wds_bridge_ssid_th = "SSID";
+scan_list_wds_bridge_sec_th = "Security";
+scan_list_wds_bridge_mac_th = "Mac Address";
+scan_list_wds_bridge_channel_th = "Channel";
+scan_list_wds_bridge_signal_th = "Signal";
+
+// wifi strength
+signal_strength_excellent = "Excellent";
+signal_strength_good = "Good";
+signal_strength_fair = "Fair";
+signal_strength_poor = "Poor";
+
+// administrator's password
+lang_page_admin_h4 = "Admin";
+lang_admin_password_label = "Admin's Password";
+lang_admin_save_button = "Save";
+
