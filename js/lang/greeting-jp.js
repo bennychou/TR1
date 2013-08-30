f71ba906c9c2064179dac9e9728fc592dc7e3fd6
= <=>
2013-08-30 11:31:07 +0800

    Modified the sentence and javascript

--- js/lang/greeting-jp.js
+++ js/lang/greeting-jp.js
@@ -27,25 +27,25 @@ lang_count_down_modal_h3 = "Progress...";
 count_down_button = "Refresh";
 
 // status
-lang_status_mode_strong = "Mode:";
-lang_status_mac_strong = "Mac Address:";
+lang_status_mode_strong = "Mode:<span id=\"status_mode_span\" class=\"label\"></span>";
+lang_status_mac_strong = "Mac Address:<span id=\"status_mac_span\" class=\"label\"></span>";
 
 // reboot and reset button
 lang_reboot_button = "Reboot";
 lang_reset_button = "Reset";
 
 // nav menu
-lang_nav_ap_bridge_a = "AP Bridge";
-lang_nav_ap_router_a = "AP Router";
-lang_nav_wds_repeater_a = "WDS Repeater";
-lang_nav_wds_bridge_a = "WDS Bridge";
-lang_nav_admin_a = "Admin";
-lang_nav_help_a = "Help";
+lang_nav_ap_bridge_a = "AP Bridge<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+lang_nav_ap_router_a = "AP Router<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+lang_nav_wds_repeater_a = "WDS Repeater<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+lang_nav_wds_bridge_a = "WDS Bridge<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+lang_nav_admin_a = "Admin<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+lang_nav_help_a = "Help<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
 
 // ap bridge mode page
 lang_page_ap_bridge_h4 = "AP Bridge Mode";
-ap_bridge_radio_non_security = "Non-Security";
-ap_bridge_radio_security = "Security";
+lang_ap_bridge_non_security_label = "<input type=\"radio\" id=\"ap_bridge_radio_non_security\" name=\"ap_bridge_radio\" value=\"non-security\" class=\"ap_bridge_radio\">Non-Security";
+lang_ap_bridge_security_label = "<input type=\"radio\" id=\"ap_bridge_radio_security\" name=\"ap_bridge_radio\" value=\"security\" class=\"ap_bridge_radio\">Security";
 lang_ap_bridge_ssid_label = "SSID";
 lang_ap_bridge_password_label = "Password";
 lang_ap_bridge_channel_label = "Channel";
@@ -53,7 +53,7 @@ lang_ap_bridge_auto_channel_option = "Auto";
 ap_bridge_radio_ht20 = "HT20";
 ap_bridge_radio_ht40 = "HT40";
 lang_ap_bridge_max_label = "Maximum";
-ap_bridge_static_input = "Static";
+lang_ap_bridge_static_label = "<input type=\"checkbox\" id=\"ap_bridge_static_input\">Static";
 lang_ap_bridge_static_ip = "IP";
 lang_ap_bridge_static_subnet = "Subnet";
 lang_ap_bridge_static_gw = "Gateway";
@@ -61,8 +61,8 @@ lang_ap_bridge_save_button = "Save";
 
 // ap router mode page
 lang_page_ap_router_h4 = "AP Router Mode";
-ap_router_radio_non_security = "Non-Security";
-ap_router_radio_security = "Security";
+lang_ap_router_non_security_label = "<input type=\"radio\" id=\"ap_router_radio_non_security\" name=\"ap_router_radio\" value=\"non-security\" class=\"ap_router_radio\">Non-Security";
+lang_ap_router_security_label = "<input type=\"radio\" id=\"ap_router_radio_security\" name=\"ap_router_radio\" value=\"security\" class=\"ap_router_radio\">Security";
 lang_ap_router_ssid_label = "SSID";
 lang_ap_router_password_label = "Password";
 lang_ap_router_channel_label = "Channel";
@@ -70,7 +70,7 @@ lang_ap_router_auto_channel_option = "Auto";
 ap_router_radio_ht20 = "HT20";
 ap_router_radio_ht40 = "HT40";
 lang_ap_router_max_label = "Maximum";
-ap_router_static_input = "Static";
+lang_ap_router_static_label = "<input type=\"checkbox\" id=\"ap_router_static_input\">Static";
 lang_ap_router_static_ip = "IP";
 lang_ap_router_static_subnet = "Subnet";
 lang_ap_router_static_gw = "Gateway";
@@ -102,3 +102,40 @@ signal_strength_poor = "Poor";
 lang_page_admin_h4 = "Admin";
 lang_admin_password_label = "Admin's Password";
 lang_admin_save_button = "Save";
+
+// mode
+lang_status_ap_bridge_mode = "AP BRIDGE";
+lang_status_ap_router_mode = "AP ROUTER";
+lang_status_wds_repeater_mode = "WDS REPEATER";
+lang_status_wds_bridge_mode = "WDS BRIDGE";
+
+// reboot dialog
+lang_dialog_reboot_title = "Reboot";
+lang_dialog_reboot_msg = "Do you want to reboot TR1?";
+
+// reset dialog
+lang_dialog_reset_title = "Reset";
+lang_dialog_reset_msg = "Do you want to reset TR1?";
+
+// progress dialog
+lang_progress_wait = "Please wait ";
+lang_progress_try_refresh = " seconds, and then try to refresh the page.";
+lang_progress_second = "s";
+
+// admin validation
+lang_password_not_correct = "Password is not correct.";
+
+// validation
+lang_enter_ssid = "Please enter the SSID.";
+lang_enter_password = "Please enter the password.";
+lang_require_eight = "Required eight characters at least.";
+lang_enter_maximum = "Please enter the maximum.";
+lang_maximum_range = "The range of the maximum is 1~150.";
+land_invalid_char = "There is an invalid character!";
+
+// alert
+lang_ip_error_head = "IP error!";
+lang_ip_error_body = "Please check your ip.";
+lang_ip_conflict_body = "You can not assign the static IP which is 10.5.1.X!";
+lang_subnet_error_head = "Subnet error!";
+lang_subnet_error_body = "Please check your subnet mask.";
--- js/lang/greeting-zh-TW.js
+++ js/lang/greeting-zh-TW.js
@@ -21,31 +21,32 @@ lang_wds_modal_password_label = "密碼";
 lang_wds_modal_wep_label = "Wep 索引";
 wds_model_cancel_button = "取消";
 wds_model_connect_button = "連接";
+wds_model_reconnect_button = "重新連接";
 
 // progerss modal
 lang_count_down_modal_h3 = "處理中...";
 count_down_button = "更新";
 
 // status
-lang_status_mode_strong = "模式：";
-lang_status_mac_strong = "Mac 位址：";
+lang_status_mode_strong = "模式：<span id=\"status_mode_span\" class=\"label\"></span>";
+lang_status_mac_strong = "Mac 位址：<span id=\"status_mac_span\" class=\"label\"></span>";
 
 // reboot and reset button
 lang_reboot_button = "重新開機";
 lang_reset_button = "回覆原廠";
 
 // nav menu
-lang_nav_ap_bridge_a = "AP 橋接";
-lang_nav_ap_router_a = "AP 路由";
-lang_nav_wds_repeater_a = "WDS Repeater";
-lang_nav_wds_bridge_a = "WDS 橋接";
-lang_nav_admin_a = "管理者";
-lang_nav_help_a = "幫助";
+lang_nav_ap_bridge_a = "AP 橋接<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+lang_nav_ap_router_a = "AP 路由<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+lang_nav_wds_repeater_a = "WDS Repeater<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+lang_nav_wds_bridge_a = "WDS 橋接<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+lang_nav_admin_a = "管理者<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+lang_nav_help_a = "幫助<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
 
 // ap bridge mode page
 lang_page_ap_bridge_h4 = "AP 橋接模式";
-ap_bridge_radio_non_security = "開放式";
-ap_bridge_radio_security = "安全性";
+lang_ap_bridge_non_security_label = "<input type=\"radio\" id=\"ap_bridge_radio_non_security\" name=\"ap_bridge_radio\" value=\"non-security\" class=\"ap_bridge_radio\">開放式";
+lang_ap_bridge_security_label = "<input type=\"radio\" id=\"ap_bridge_radio_security\" name=\"ap_bridge_radio\" value=\"security\" class=\"ap_bridge_radio\">安全性";
 lang_ap_bridge_ssid_label = "SSID 名稱";
 lang_ap_bridge_password_label = "密碼";
 lang_ap_bridge_channel_label = "頻道";
@@ -53,7 +54,7 @@ lang_ap_bridge_auto_channel_option = "自動";
 ap_bridge_radio_ht20 = "HT20";
 ap_bridge_radio_ht40 = "HT40";
 lang_ap_bridge_max_label = "最大連接數";
-ap_bridge_static_input = "固定 IP";
+lang_ap_bridge_static_label = "<input type=\"checkbox\" id=\"ap_bridge_static_input\">固定 IP";
 lang_ap_bridge_static_ip = "IP 位址";
 lang_ap_bridge_static_subnet = "子網域遮罩";
 lang_ap_bridge_static_gw = "預設閘道";
@@ -61,8 +62,8 @@ lang_ap_bridge_save_button = "儲存";
 
 // ap router mode page
 lang_page_ap_router_h4 = "AP 路由模式";
-ap_router_radio_non_security = "開放式";
-ap_router_radio_security = "安全性";
+lang_ap_router_non_security_label = "<input type=\"radio\" id=\"ap_router_radio_non_security\" name=\"ap_router_radio\" value=\"non-security\" class=\"ap_router_radio\">開放式";
+lang_ap_router_security_label = "<input type=\"radio\" id=\"ap_router_radio_security\" name=\"ap_router_radio\" value=\"security\" class=\"ap_router_radio\">安全性";
 lang_ap_router_ssid_label = "SSID 名稱";
 lang_ap_router_password_label = "密碼";
 lang_ap_router_channel_label = "頻道";
@@ -70,7 +71,7 @@ lang_ap_router_auto_channel_option = "自動";
 ap_router_radio_ht20 = "HT20";
 ap_router_radio_ht40 = "HT40";
 lang_ap_router_max_label = "最大連接數";
-ap_router_static_input = "固定 IP";
+lang_ap_router_static_label = "<input type=\"checkbox\" id=\"ap_router_static_input\">固定 IP";
 lang_ap_router_static_ip = "IP 位址";
 lang_ap_router_static_subnet = "子網域遮罩";
 lang_ap_router_static_gw = "預設閘道";
@@ -102,3 +103,40 @@ signal_strength_poor = "很弱";
 lang_page_admin_h4 = "管理者";
 lang_admin_password_label = "管理者密碼";
 lang_admin_save_button = "儲存";
+
+// mode
+lang_status_ap_bridge_mode = "AP 橋接";
+lang_status_ap_router_mode = "AP 路由";
+lang_status_wds_repeater_mode = "WDS REPEATER";
+lang_status_wds_bridge_mode = "WDS 橋接";
+
+// reboot dialog
+lang_dialog_reboot_title = "重新開機";
+lang_dialog_reboot_msg = "您確定要重新開機嗎？";
+
+// reset dialog
+lang_dialog_reset_title = "回覆原廠";
+lang_dialog_reset_msg = "您確定要回覆原廠嗎？";
+
+// progress dialog
+lang_progress_wait = "請等待 ";
+lang_progress_try_refresh = " 秒後，更新畫面。";
+lang_progress_second = "秒";
+
+// admin validation
+lang_password_not_correct = "密碼錯誤！";
+
+// validation
+lang_enter_ssid = "請輸入 SSID";
+lang_enter_password = "請輸入密碼";
+lang_require_eight = "至少輸入八個字元！";
+lang_enter_maximum = "請輸入最大連接數";
+lang_maximum_range = "超出範圍，最大連接數的範圍：1~150";
+land_invalid_char = "錯誤字元！";
+
+// alert
+lang_ip_error_head = "IP 錯誤";
+lang_ip_error_body = "請檢查您的IP";
+lang_ip_conflict_body = "您設定的 IP 不能與 10.5.1.1 同網段";
+lang_subnet_error_head = "子網域遮罩錯誤";
+lang_subnet_error_body = "請檢查您的子網域遮罩";
