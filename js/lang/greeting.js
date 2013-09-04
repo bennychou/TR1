f7fadb18c81213b98a9581158990f92e0da4a996
Benny <bennychou7573@hotmail.com>
2013-09-04 18:24:09 +0800

    Modified details
    1. WEP Key
    2. Corrected language files
    3. Deleted Japanese

--- js/lang/greeting.js
+++ js/lang/greeting.js
@@ -1,11 +1,10 @@
 lang_option_1 = "English";
-lang_option_2 = "日本語";
-lang_option_3 = "繁體中文";
-lang_option_4 = "简体中文";
+lang_option_2 = "繁體中文";
+lang_option_3 = "简体中文";
 
 // admin modal
 lang_admin_modal_h3 = "Login";
-lang_admin_modal_label = "Admin's Password";
+lang_admin_modal_label = "Password";
 lang_admin_modal_button = "Confirm";
 
 // confirm reset or reboot modal
@@ -18,7 +17,7 @@ lang_wds_modal_security_label = "Security";
 lang_wds_modal_mac_label = "Mac Address";
 lang_wds_modal_channel_label = "Channel";
 lang_wds_modal_password_label = "Password";
-lang_wds_modal_wep_label = "Wep Key";
+lang_wds_modal_wep_label = "WEP Key";
 wds_model_cancel_button = "Cancel";
 wds_model_connect_button = "Connect";
 
@@ -35,15 +34,15 @@ lang_reboot_button = "Reboot";
 lang_reset_button = "Reset";
 
 // nav menu
-lang_nav_ap_bridge_a = "AP Bridge<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
-lang_nav_ap_router_a = "AP Router<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
-lang_nav_wds_repeater_a = "WDS Repeater<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
-lang_nav_wds_bridge_a = "WDS Bridge<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+lang_nav_ap_bridge_a = "AP Mode<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+lang_nav_ap_router_a = "Router Mode<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+lang_nav_wds_repeater_a = "Repeater Mode<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+lang_nav_wds_bridge_a = "Client Mode<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
 lang_nav_admin_a = "Admin<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
 lang_nav_help_a = "Help<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
 
 // ap bridge mode page
-lang_page_ap_bridge_h4 = "AP Bridge Mode";
+lang_page_ap_bridge_h4 = "AP Mode";
 lang_ap_bridge_non_security_label = "<input type=\"radio\" id=\"ap_bridge_radio_non_security\" name=\"ap_bridge_radio\" value=\"non-security\" class=\"ap_bridge_radio\">Non-Security";
 lang_ap_bridge_security_label = "<input type=\"radio\" id=\"ap_bridge_radio_security\" name=\"ap_bridge_radio\" value=\"security\" class=\"ap_bridge_radio\">Security";
 lang_ap_bridge_ssid_label = "SSID";
@@ -60,7 +59,7 @@ lang_ap_bridge_static_gw = "Gateway";
 lang_ap_bridge_save_button = "Save";
 
 // ap router mode page
-lang_page_ap_router_h4 = "AP Router Mode";
+lang_page_ap_router_h4 = "Router Mode";
 lang_ap_router_non_security_label = "<input type=\"radio\" id=\"ap_router_radio_non_security\" name=\"ap_router_radio\" value=\"non-security\" class=\"ap_router_radio\">Non-Security";
 lang_ap_router_security_label = "<input type=\"radio\" id=\"ap_router_radio_security\" name=\"ap_router_radio\" value=\"security\" class=\"ap_router_radio\">Security";
 lang_ap_router_ssid_label = "SSID";
@@ -77,7 +76,7 @@ lang_ap_router_static_gw = "Gateway";
 lang_ap_router_save_button = "Save";
 
 // wds repeater mode
-lang_page_wds_repeater_h4 = "WDS Repeater Mode";
+lang_page_wds_repeater_h4 = "Repeater Mode";
 scan_list_wds_repeater_ssid_th = "SSID";
 scan_list_wds_repeater_sec_th = "Security";
 scan_list_wds_repeater_mac_th = "Mac Address";
@@ -85,7 +84,7 @@ scan_list_wds_repeater_channel_th = "Channel";
 scan_list_wds_repeater_signal_th = "Signal";
 
 // wds bridge mode
-lang_page_wds_bridge_h4 = "WDS Bridge Mode";
+lang_page_wds_bridge_h4 = "Client Mode";
 scan_list_wds_bridge_ssid_th = "SSID";
 scan_list_wds_bridge_sec_th = "Security";
 scan_list_wds_bridge_mac_th = "Mac Address";
@@ -100,22 +99,22 @@ signal_strength_poor = "Poor";
 
 // administrator's password
 lang_page_admin_h4 = "Admin";
-lang_admin_password_label = "Admin's Password";
+lang_admin_password_label = "Password";
 lang_admin_save_button = "Save";
 
 // mode
-lang_status_ap_bridge_mode = "AP BRIDGE";
-lang_status_ap_router_mode = "AP ROUTER";
-lang_status_wds_repeater_mode = "WDS REPEATER";
-lang_status_wds_bridge_mode = "WDS BRIDGE";
+lang_status_ap_bridge_mode = "AP MODE";
+lang_status_ap_router_mode = "ROUTER MODE";
+lang_status_wds_repeater_mode = "REPEATER MODE";
+lang_status_wds_bridge_mode = "CLIENT MODE";
 
 // reboot dialog
 lang_dialog_reboot_title = "Reboot";
-lang_dialog_reboot_msg = "Do you want to reboot TR1?";
+lang_dialog_reboot_msg = "Do you want to reboot R101?";
 
 // reset dialog
 lang_dialog_reset_title = "Reset";
-lang_dialog_reset_msg = "Do you want to reset TR1?";
+lang_dialog_reset_msg = "Do you want to reset R101?";
 
 // progress dialog
 lang_progress_wait = "Please wait ";
@@ -128,14 +127,14 @@ lang_password_not_correct = "Password is not correct.";
 // validation
 lang_enter_ssid = "Please enter the SSID.";
 lang_enter_password = "Please enter the password.";
-lang_require_eight = "Required eight characters at least.";
+lang_require_eight = "Minimum 8 characters.";
 lang_enter_maximum = "Please enter the maximum.";
 lang_maximum_range = "The range of the maximum is 1~150.";
 land_invalid_char = "There is an invalid character!";
 
 // alert
 lang_ip_error_head = "IP error!";
-lang_ip_error_body = "Please check your ip.";
-lang_ip_conflict_body = "You can not assign the static IP which is 192.168.16.X!";
+lang_ip_error_body = "Please check your IP.";
+lang_ip_conflict_body = "You can not assign the static IP as 192.168.16.X!";
 lang_subnet_error_head = "Subnet error!";
 lang_subnet_error_body = "Please check your subnet mask.";
