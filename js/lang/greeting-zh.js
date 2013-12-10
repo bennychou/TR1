57c0a58d8c84cebf0d5f48bea69e0e7507a96846
Benny <bennychou7573@hotmail.com>
2013-12-10 17:10:05 +0800

    Modified the timing of change language
    1. Added the localization sentence
    2. Modified the code for performance

---
+++ js/lang/greeting-zh.js
@@ -0,0 +1,168 @@
+lang_option_1 = "English";
+lang_option_2 = "繁體中文";
+lang_option_3 = "简体中文";
+lang_option_4 = "日本語";
+
+// admin modal
+lang_admin_modal_h3 = "登入";
+lang_admin_modal_label = "密碼";
+lang_admin_modal_button = "確定";
+
+// confirm reset or reboot modal
+lang_confirm_modal_cancel_button = "取消";
+lang_confirm_modal_confirm_button = "確定";
+
+// WDS connect modal
+lang_wds_modal_signal_label = "訊號";
+lang_wds_modal_security_label = "安全";
+lang_wds_modal_mac_label = "Mac 位址";
+lang_wds_modal_channel_label = "頻道";
+lang_wds_modal_password_label = "密碼";
+lang_wds_modal_wep_label = "WEP 密鑰";
+wds_model_cancel_button = "取消";
+wds_model_connect_button = "連線";
+wds_model_reconnect_button = "重新連線";
+
+// progerss modal
+lang_count_down_modal_h3 = "進行中...";
+count_down_button = "重新整理";
+
+// status
+lang_status_mode_strong = "模式：<span id=\"status_mode_span\" class=\"label\"></span>";
+lang_status_mac_strong = "Mac 位址：<span id=\"status_mac_span\" class=\"label\"></span>";
+
+// reboot and reset button
+lang_reboot_button = "重新啟動";
+lang_reset_button = "恢復出廠預設";
+
+// nav menu
+lang_nav_ap_bridge_a = "AP 模式<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+lang_nav_ap_router_a = "路由模式<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+lang_nav_pppoe_a = "PPPoE 模式<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+lang_nav_wds_repeater_a = "中繼模式<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+lang_nav_wds_bridge_a = "客戶端模式<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+lang_nav_admin_a = "管理者<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+lang_nav_firmware_a = "韌體升級<img src=\"img/arrow.png\" class=\"menu_arrow\"/>";
+
+// ap bridge mode page
+lang_page_ap_bridge_h4 = "AP 模式";
+lang_ap_bridge_non_security_label = "<input type=\"radio\" id=\"ap_bridge_radio_non_security\" name=\"ap_bridge_radio\" value=\"non-security\" class=\"ap_bridge_radio\">開放模式 (無密碼)";
+lang_ap_bridge_security_label = "<input type=\"radio\" id=\"ap_bridge_radio_security\" name=\"ap_bridge_radio\" value=\"security\" class=\"ap_bridge_radio\">安全模式 (有密碼)";
+lang_ap_bridge_ssid_label = "服務設定識別碼";
+lang_ap_bridge_password_label = "密碼";
+lang_ap_bridge_channel_label = "頻道";
+lang_ap_bridge_auto_channel_option = "自動";
+ap_bridge_radio_ht20 = "HT20";
+ap_bridge_radio_ht40 = "HT40";
+lang_ap_bridge_max_label = "DHCP 最大值";
+lang_ap_bridge_static_label = "<input type=\"checkbox\" id=\"ap_bridge_static_input\">固定 IP";
+lang_ap_bridge_static_ip = "IP";
+lang_ap_bridge_static_subnet = "子網路";
+lang_ap_bridge_static_gw = "閘道";
+lang_ap_bridge_save_button = "儲存";
+
+// ap router mode page
+lang_page_ap_router_h4 = "路由模式";
+lang_ap_router_non_security_label = "<input type=\"radio\" id=\"ap_router_radio_non_security\" name=\"ap_router_radio\" value=\"non-security\" class=\"ap_router_radio\">開放模式 (無密碼)";
+lang_ap_router_security_label = "<input type=\"radio\" id=\"ap_router_radio_security\" name=\"ap_router_radio\" value=\"security\" class=\"ap_router_radio\">安全模式 (有密碼)";
+lang_ap_router_ssid_label = "服務設定識別碼";
+lang_ap_router_password_label = "密碼";
+lang_ap_router_channel_label = "頻道";
+lang_ap_router_auto_channel_option = "自動";
+ap_router_radio_ht20 = "HT20";
+ap_router_radio_ht40 = "HT40";
+lang_ap_router_max_label = "DHCP 最大值";
+lang_ap_router_static_label = "<input type=\"checkbox\" id=\"ap_router_static_input\">固定 IP";
+lang_ap_router_static_ip = "IP";
+lang_ap_router_static_subnet = "子網路";
+lang_ap_router_static_gw = "閘道";
+lang_ap_router_save_button = "儲存";
+
+// wds repeater mode
+lang_page_wds_repeater_h4 = "中繼模式";
+scan_list_wds_repeater_ssid_th = "服務設定識別碼";
+scan_list_wds_repeater_sec_th = "安全";
+scan_list_wds_repeater_mac_th = "Mac 位址";
+scan_list_wds_repeater_channel_th = "頻道";
+scan_list_wds_repeater_signal_th = "訊號";
+
+// wds bridge mode
+lang_page_wds_bridge_h4 = "客戶端模式";
+scan_list_wds_bridge_ssid_th = "服務設定識別碼";
+scan_list_wds_bridge_sec_th = "安全";
+scan_list_wds_bridge_mac_th = "Mac 位址";
+scan_list_wds_bridge_channel_th = "頻道";
+scan_list_wds_bridge_signal_th = "訊號";
+
+// firmware upgrade
+lang_page_firmware_h4 = "韌體升級";
+lang_firm_upgrapde_label = "韌體映像檔";
+lang_firmware_upgrade_button = "升級";
+lang_firmware_error_head = "升級失敗！";
+lang_firmware_error_body = "請檢查映像檔並再試一次。"
+alert_firmware_change_head_h4 = "注意";
+alert_firmware_change_body_p = "請變換為 AP 模式或路由模式。";
+
+// wifi strength
+signal_strength_excellent = "極佳";
+signal_strength_good = "好";
+signal_strength_fair = "一般";
+signal_strength_poor = "差";
+
+// administrator's password
+lang_page_admin_h4 = "管理者";
+lang_admin_password_label = "密碼";
+lang_admin_save_button = "儲存";
+
+// mode
+lang_status_ap_bridge_mode = "AP 模式";
+lang_status_ap_router_mode = "路由模式";
+lang_status_pppoe_mode = "PPPoE 模式";
+lang_status_wds_repeater_mode = "中繼模式";
+lang_status_wds_bridge_mode = "客戶端模式";
+
+// reboot dialog
+lang_dialog_reboot_title = "重新啟動";
+lang_dialog_reboot_msg = "請問是否重新啟動 R101？";
+
+// reset dialog
+lang_dialog_reset_title = "恢復出廠預設";
+lang_dialog_reset_msg = "請問是否恢復 R101 預設？";
+
+// progress dialog
+lang_progress_wait = "請稍候 ";
+lang_progress_try_refresh = " 秒，將重新更新頁面。";
+lang_progress_second = "秒";
+
+// admin validation
+lang_password_not_correct = "密碼錯誤！";
+
+// validation
+lang_enter_ssid = "請輸入服務設定識別碼";
+lang_enter_username = "請輸入服務使用者名稱";
+lang_enter_password = "請輸入密碼";
+lang_require_eight = "至少八字元！";
+lang_enter_maximum = "請輸入最大值";
+lang_maximum_range = "最大值範圍：1~150";
+land_invalid_char = "這是一個無效字元！";
+
+// alert
+lang_ip_error_head = "IP 錯誤";
+lang_ip_error_body = "請檢查您的 IP";
+lang_ip_conflict_body = "您不能設定使用 192.168.16.X 這類 IP 地址";
+lang_subnet_error_head = "子網路錯誤";
+lang_subnet_error_body = "請確認您的子網遮罩";
+
+// pppoe
+lang_page_pppoe_h4 = "PPPoE 模式";
+lang_pppoe_username_label = "使用者名稱";
+lang_pppoe_pwd_label = "密碼";
+lang_pppoe_enable_label = "<input type=\"radio\" id=\"pppoe_radio_enable\" name=\"pppoe_radio\" value=\"security\" class=\"pppoe_radio\">啟動";
+lang_pppoe_disable_label = "<input type=\"radio\" id=\"pppoe_radio_disable\" name=\"pppoe_radio\" value=\"security\" class=\"pppoe_radio\">關閉";
+lang_pppoe_status_label = "狀態";
+lang_pppoe_status_connected = "已連接";
+lang_pppoe_status_disconnected = "未連接";
+lang_pppoe_status_processing = "處理中...";
+lang_pppoe_save_button = "儲存";
+alert_pppoe_head_h4 = "已儲存";
+alert_pppoe_body_p = "您已成功儲存 PPPoE 模式之設定。"
